30abc31084cb10161edfa8cb3d039bada0b5f8ed
Kieran <kieran.webdevelopment@gmail.com>
2023-09-27 17:50:20 +0100

    complete first 3 challenges

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,31 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  // check correct types of arguments
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return 'numbers must be entered as numbers';
+  }
+  if (typeof operator !== 'string') {
+    return 'operator must be a string';
+  }
+  // check valid operator
+  if (
+    operator === '+' ||
+    operator === '-' ||
+    operator === '*' ||
+    operator === '/'
+  ) {
+    // perform calculation
+    if (operator === '+') {
+      return num1 + num2;
+    } else if (operator === '-') {
+      return num1 - num2;
+    } else if (operator === '*') {
+      return num1 * num2;
+    } else if (operator === '/') {
+      return num1 / num2;
+    }
+  } else {
+    return 'operator must be +, -, *, or /';
+  }
+}
 
 module.exports = calculator;
