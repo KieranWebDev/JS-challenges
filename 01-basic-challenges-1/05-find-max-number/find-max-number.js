969ae28fcc88ded7b2d05fab2fe54d975067bfac
Kieran <kieran.webdevelopment@gmail.com>
2023-09-28 11:00:27 +0100

    completed challenge 5 with 2 possible solutions

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,20 @@
-function findMaxNumber() {}
+// solution 1
+// function findMaxNumber(arr) {
+//   // use sort method to sort array lowest to highest
+//   // return last item in array
+//   let sortedArr = arr.sort((a, b) => a - b);
+//   return sortedArr[sortedArr.length - 1];
+// }
+
+// solution 2
+function findMaxNumber(arr) {
+  let highestNum = 0;
+  for (let item of arr) {
+    if (item > highestNum) {
+      highestNum = item;
+    }
+  }
+  return highestNum;
+}
 
 module.exports = findMaxNumber;
