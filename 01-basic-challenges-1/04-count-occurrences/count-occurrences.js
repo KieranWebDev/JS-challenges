f59f219011346f9e45191c60b00d937d7db9836f
Kieran <kieran.webdevelopment@gmail.com>
2023-09-28 10:25:45 +0100

    added another possible solution for excercise 4 using map method

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,16 +1,31 @@
 function countOccurrences(str, char) {
+  //   approach 1
   // create let to store count
   // loop through string
-  //   if str[i] === char then count +1
+  // if str[i] === char then count +1
   // return count
 
-  let count = 0;
+  //   const lowerCaseStr = str.toLowerCase();
+  //   let count = 0;
 
-  for (let item of str) {
-    if (item === char) {
-      count += 1;
-    }
-  }
+  //   for (let item of lowerCaseStr) {
+  //     if (item === char) {
+  //       count += 1;
+  //     }
+  //   }
+  //   return count;
+
+  //   appeoach 2
+  // split str
+  // use map method to loop through array and compare each item to char
+  //   add 1 to count if equal
+  // return count
+  let count = 0;
+  let lowercaseStrArr = str
+    .toLowerCase()
+    .split('')
+    .map((item) => (item === char ? count++ : item));
+  //   console.log(lowercaseStrArr);
   return count;
 }
 
