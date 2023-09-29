0ce98686875497be07371a8791a700464d694b2f
Kieran <kieran.webdevelopment@gmail.com>
2023-09-29 16:46:31 +0100

    completed challenge 6

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,9 @@
-function titleCase() {}
+function titleCase(str) {
+  let capitalisedStr = str
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.slice(1, word.length))
+    .join(' ');
+  return capitalisedStr;
+}
 
 module.exports = titleCase;
