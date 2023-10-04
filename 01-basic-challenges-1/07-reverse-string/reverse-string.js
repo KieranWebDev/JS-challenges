17e71f1aff4b8e437a37aae00a53e8dbfdfb0eb4
Kieran <kieran.webdevelopment@gmail.com>
2023-10-04 19:01:10 +0100

    added another solution for challenge 7

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,5 +1,13 @@
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+// }
 function reverseString(str) {
-  return str.split('').reverse().join('');
+  let reversedStr = [];
+
+  for (let i = str.length; i >= 0; i--) {
+    reversedStr.push(str[i]);
+  }
+  return reversedStr.join('');
 }
 
 module.exports = reverseString;
