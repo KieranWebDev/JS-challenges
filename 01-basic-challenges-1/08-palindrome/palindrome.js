6847dfe11f9b1e413d3911474fce5188fb88c014
Kieran <kieran.webdevelopment@gmail.com>
2023-10-06 15:51:32 +0100

    improved challenge 8 solution by using regex

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,12 +1,21 @@
+// function isPalindrome(str) {
+//   let formattedStr = str.toLowerCase().split(' ').join('');
+
+//   formattedStr.includes(',')
+//     ? (formattedStr = formattedStr.split(',').join(''))
+//     : (formattedStr = formattedStr);
+
+//   const reversedStr = formattedStr.split('').reverse().join('');
+//   console.log(formattedStr, reversedStr);
+//   return formattedStr === reversedStr;
+// }
+
 function isPalindrome(str) {
-  let formattedStr = str.toLowerCase().split(' ').join('');
+  // reg expression to remove commas and spaces
+  let formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-  formattedStr.includes(',')
-    ? (formattedStr = formattedStr.split(',').join(''))
-    : (formattedStr = formattedStr);
+  let reversedStr = formattedStr.split('').reverse().join('');
 
-  const reversedStr = formattedStr.split('').reverse().join('');
-  console.log(formattedStr, reversedStr);
   return formattedStr === reversedStr;
 }
 
