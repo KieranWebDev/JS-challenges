97c16b0b882003c6a3731901f6f31d85a88e208f
Kieran <kieran.webdevelopment@gmail.com>
2023-10-06 15:42:19 +0100

    completed challenge 8

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,13 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let formattedStr = str.toLowerCase().split(' ').join('');
+
+  formattedStr.includes(',')
+    ? (formattedStr = formattedStr.split(',').join(''))
+    : (formattedStr = formattedStr);
+
+  const reversedStr = formattedStr.split('').reverse().join('');
+  console.log(formattedStr, reversedStr);
+  return formattedStr === reversedStr;
+}
 
 module.exports = isPalindrome;
