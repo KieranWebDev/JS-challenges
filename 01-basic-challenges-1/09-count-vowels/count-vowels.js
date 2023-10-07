355fbcf5e0580f1eef71d53145d28c6488e2681e
Kieran <kieran.webdevelopment@gmail.com>
2023-10-07 11:11:06 +0100

    completed challenge 9 using for of loop

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,13 +1,31 @@
-function countVowels(str) {
-  let vowel = /[aeiouAEIOU]/g;
-  const vowelsArr = str.match(vowel);
-  return vowelsArr.length;
-}
+// function countVowels(str) {
+//   // create regex to check for vowels, uppercase and lowercase
+//   // created vowels arr and use match method
+//   // return vowelsArr.length
+//   let vowel = /[aeiouAEIOU]/g;
+//   const vowelsArr = str.match(vowel);
+//   return vowelsArr.length;
+// }
 
 function countVowels(str) {
-  let vowel = /[aeiouAEIOU]/g;
-  const vowelsArr = str.match(vowel);
-  return vowelsArr.length;
+  // convert str to lowercase
+  // create count var
+  // for loop on str
+  // if str matches a,e,i,o,u then add 1 to count
+  let count = 0;
+  let formattedStr = str.toLowerCase();
+  for (let char of formattedStr) {
+    if (
+      char === 'a' ||
+      char === 'e' ||
+      char === 'i' ||
+      char === 'o' ||
+      char === 'u'
+    ) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
