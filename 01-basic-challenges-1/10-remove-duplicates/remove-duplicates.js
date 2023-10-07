ac7786d51e88438c3faec20f882b674e3f9b5305
Kieran <kieran.webdevelopment@gmail.com>
2023-10-07 11:24:52 +0100

    solution for challenge 10 using for loop

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,6 +1,20 @@
+// function removeDuplicates(arr) {
+//   let set = new Set(arr);
+//   return [...set];
+// }
 function removeDuplicates(arr) {
-  let set = new Set(arr);
-  return [...set];
+  // create empty arr to hold uniqe vals
+  // loop though original arr
+  // use includes method. if uniquevals arr !== arr[i] then push value into new array
+  // return new arr
+
+  let uniqueItems = [];
+  for (let item of arr) {
+    if (!uniqueItems.includes(item)) {
+      uniqueItems.push(item);
+    }
+  }
+  return uniqueItems;
 }
 
 module.exports = removeDuplicates;
