9e78385e1ec17b0f0a45b23cd63a00d2a72f5d3f
Kieran <kieran.webdevelopment@gmail.com>
2023-10-07 11:01:59 +0100

    completed challenge 9 using regex

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,13 @@
-function countVowels() {}
+function countVowels(str) {
+  let vowel = /[aeiouAEIOU]/g;
+  const vowelsArr = str.match(vowel);
+  return vowelsArr.length;
+}
+
+function countVowels(str) {
+  let vowel = /[aeiouAEIOU]/g;
+  const vowelsArr = str.match(vowel);
+  return vowelsArr.length;
+}
 
 module.exports = countVowels;
