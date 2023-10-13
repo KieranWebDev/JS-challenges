a3ce0f6929b9d018f752c4e7f4fa1ff915925e5d
Kieran <kieran.webdevelopment@gmail.com>
2023-10-13 11:41:01 +0100

    solved challenge 14

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,18 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  // set count var to 1
+  // sort array in acsending order
+  // loop through arr
+  // every iteration increase count by 1
+  // arr[i] doesn not match count then return arr[i]
+
+  let count = 0;
+  let sortedArr = arr.sort((a, b) => a - b);
+  for (let item of sortedArr) {
+    count++;
+    if (count !== item) {
+      return count;
+    }
+  }
+}
 
 module.exports = findMissingNumber;
