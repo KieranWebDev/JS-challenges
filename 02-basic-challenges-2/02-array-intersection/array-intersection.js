8204922002a80e59183ff17975d17e681f2de380
Kieran <kieran.webdevelopment@gmail.com>
2023-10-13 11:12:43 +0100

    solved challenge 12

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  // loop throught firts array
+  // if second array does not include() arr one item then push to new array
+  // return new array
+  let uniqueArray = [];
+
+  for (let item of arr1) {
+    if (arr2.includes(item)) {
+      uniqueArray.push(item);
+    }
+  }
+  return uniqueArray;
+}
 
 module.exports = arrayIntersection;
