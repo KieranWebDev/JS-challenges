33642362612e7050727e36cf1ba562e728dd4557
Kieran <kieran.webdevelopment@gmail.com>
2023-10-13 11:21:54 +0100

    completed alternative solition for challenge 12 using map method

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,15 +1,26 @@
-function arrayIntersection(arr1, arr2) {
-  // loop throught firts array
-  // if second array does not include() arr one item then push to new array
-  // return new array
-  let uniqueArray = [];
+// function arrayIntersection(arr1, arr2) {
+//   // loop throught firts array
+//   // if second array does not include() arr one item then push to new array
+//   // return new array
+//   let uniqueArray = [];
 
-  for (let item of arr1) {
-    if (arr2.includes(item)) {
-      uniqueArray.push(item);
-    }
-  }
-  return uniqueArray;
+//   for (let item of arr1) {
+//     if (arr2.includes(item)) {
+//       uniqueArray.push(item);
+//     }
+//   }
+//   return uniqueArray;
+// }
+
+function arrayIntersection(arr1, arr2) {
+  // map through arr1
+  // if arr2 includes item in arr push item to new array
+
+  const newArr = [];
+
+  arr1.map((item) => (arr2.includes(item) ? newArr.push(item) : item));
+
+  return newArr;
 }
 
 module.exports = arrayIntersection;
